9399034b47504b5ef4299f72518951db4de72a08
clarkjanndy <cjcasol22@gmail.com>
2023-06-13 18:26:10 +0800

    migrate to MySQL

--- CIMS/settings.py
+++ CIMS/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'CIMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'mysql',
+        'NAME': 'CIMS',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 # DATABASES = {
